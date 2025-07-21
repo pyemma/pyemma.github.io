621448881569a0b3605e217edd8425404c27c5a9
Yang Pei <pyemma1991@gmail.com>
2025-07-20 19:12:10 -0700

    Add PyTorch Training Book Summary

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1751316815',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1753063914',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
